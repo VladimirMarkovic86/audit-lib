40430af33f168f230ac0f58c1d2a97b7324f4c4b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-29 17:55:24 +0100

    Task 30: Implemented chat on common (client, middle, server) projects

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.8"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.9"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.3"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.6"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.7"]
                  ]
 
   :min-lein-version "2.0.0"
