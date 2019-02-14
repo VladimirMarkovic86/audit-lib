6a53f42065fc360a9b996094cc36337271061810
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-14 17:48:05 +0100

    Task 54: Chat works through websocket connection now, and implemented Range header support for audio/video streaming.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.10"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.11"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.3"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.7"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.8"]
                  ]
 
   :min-lein-version "2.0.0"
