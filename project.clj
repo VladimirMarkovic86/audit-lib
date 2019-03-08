883cdfb615f72c178c31b8fd655e8b7e0728a037
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-08 17:30:50 +0100

    Task 62: Refactored common-server, separated configuration for server and client side.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.13"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.14"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.4"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.9"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.5"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.10"]
                  ]
 
   :min-lein-version "2.0.0"
