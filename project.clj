c5ea720f41134b884b1ce06a0a6862a585fc6c17
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-27 11:49:17 +0100

    Task 68: Implemented three different scrapers which can be adapted to clients requests.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.19"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.20"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.14"]
                  ]
 
