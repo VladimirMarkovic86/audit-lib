7212a4e4e21a51d25385403f291cd4469932f659
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-28 16:47:46 +0100

    Task 64: Implemented tests for mongo-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.20"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.21"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.6"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.14"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.15"]
                  ]
 
   :min-lein-version "2.0.0"
