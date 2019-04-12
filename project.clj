53e942ee181aabf0cdf2fe006d0cac74f6d3c8fb
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-12 16:11:45 +0200

    Task 65: Implemented pdf latex reports.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.23"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.24"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.7"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.17"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.18"]
                  ]
 
   :min-lein-version "2.0.0"
