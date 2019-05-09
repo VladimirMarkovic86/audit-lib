7b229556fa7363a8b66984aa94609df764d12b1d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-09 17:28:51 +0200

    Task 66: Implemented charts in svg-lib and used it in sample-client.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.24"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.25"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.18"]
                  ]
 
