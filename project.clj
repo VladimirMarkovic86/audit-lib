e9735e132a451e01be5e76033d0958fd26ccd5a5
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 12:49:44 +0200

    Task 88: Removed public folder from tested clojurescript projects packed jar file.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.33"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.34"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.25"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
                  ]
 
   :min-lein-version "2.0.0"
