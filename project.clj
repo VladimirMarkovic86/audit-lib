4f1aacab4f5122b04f221f6914026f6928dc0634
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:54:17 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,16 +1,19 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.34"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.35"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"])
+  :test-paths ["test/clj"]
+  
+  :jar-exclusions [#"README.md$"
+                   #"LICENSE$"])
 
