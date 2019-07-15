2115a296f45e82333b0cda744dd83079d6bfcd6a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:01:37 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,19 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.35"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.36"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.28"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  
-  :jar-exclusions [#"README.md$"
-                   #"LICENSE$"])
+  :test-paths ["test/clj"])
 
