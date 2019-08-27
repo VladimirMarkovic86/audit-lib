a8216a2caa813e0f6e4368c1b6fc8edda06e7372
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-27 12:18:41 +0200

    Task 5: Implemented calendar component in common-client namespace.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.38"
+(defproject org.clojars.vladimirmarkovic86/audit-lib "0.1.39"
   :description "Audit library"
   :url "http://github.com/VladimirMarkovic86/audit-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.13"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.14"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.30"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.31"]
                  ]
 
   :min-lein-version "2.0.0"
